9e540c087af7a1aca4941ffff7c99718d51d0d55
Cyril <131610338+P4radoxxx@users.noreply.github.com>
2024-03-27 21:05:56 +0100

    Update Offsets.hpp

--- nika-fixed/Offsets.hpp
+++ nika-fixed/Offsets.hpp
@@ -1,27 +1,37 @@
-    //GameVersion=v3.0.59.33
-    //LastUpdated=06/03/2024
-    #pragma once
-    //core
-    constexpr long OFF_REGION = 0x140000000; //[Static]->Region
-    constexpr long OFF_LEVEL = 0x16f9090; //[Miscellaneous]->LevelName
-    constexpr long OFF_LOCAL_PLAYER = 0x2182bf8; //[Miscellaneous]->LocalPlayer o
-    constexpr long OFF_ENTITY_LIST = 0x1DD40F8; //[Miscellaneous]->cl_entitylist o
-     
-    //buttons
-    constexpr long OFF_INATTACK = 0x073f2798;                     //[Buttons]->in_attack
-    constexpr long OFF_IN_JUMP = 0x073f2878;                      //[Buttons]->in_jump
-    constexpr long OFF_IN_DUCK = 0x073f2978;                      //[Buttons]->in_duck
-    constexpr long OFF_IN_FORWARD = 0x073f26c8;                //[Buttons]->in_forward
-    constexpr long OFF_TRAVERSAL_START_TIME = 0x2af0;          //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
-    constexpr long OFF_TRAVERSAL_PROGRESS = 0x2aec;            //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
-    constexpr long OFF_WALL_RUN_START_TIME = 0x3584;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
-    constexpr long OFF_WALL_RUN_CLEAR_TIME = 0x3588;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunClearTime
-    constexpr long OFF_GRAPPLE_ACTIVE = 0x1e38;              //[RecvTable.DT_NPC_Titan]       Not sure
-    constexpr long OFF_GRAPPLE = 0x1da8;                      //[RecvTable.DT_NPC_Titan]        Not sure
-    constexpr long OFF_GRAPPLE_ATTACHED = 0x0048;              // [RecvTable.DT_GrappleData]       Not sure
-     
-    // player
-    constexpr long OFF_HEALTH = 0x0318;                           //[RecvTable.DT_Player]->m_iHealth
+#pragma once
+//https://www.unknowncheats.me/forum/apex-legends/319804-apex-legends-reversal-structs-offsets.html
+ 
+//GameVersion = v3.0.62.29
+
+// Core    
+constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
+constexpr long OFF_LEVEL = 0x16f9090;                         //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x2182bf8;                  //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1DD40F8;                   //[Miscellaneous]->cl_entitylist
+constexpr long OFF_NAME_INDEX = 0x38;                         //nameIndex
+constexpr long OFF_NAME_LIST = 0xC5FEC00;                     //[Miscellaneous]->NameList
+constexpr long OFF_GAMEMODE = 0x021ba460;                     //mp_gamemode
+constexpr long OFF_SQUAD_ID = 0x0334;                          //m_squadID
+ 
+// HUD
+constexpr long OFF_VIEWRENDER = 0x73f14f8;                    //[Miscellaneous]->ViewRenderer
+constexpr long OFF_VIEWMATRIX = 0x11a350;                     //[Miscellaneous]->ViewMatrix
+ 
+// Buttons
+constexpr long OFF_INATTACK = 0x073f2798;                     //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x073f2878;                      //[Buttons]->in_jump
+constexpr long OFF_IN_DUCK = 0x073f2978;                      //[Buttons]->in_duck
+constexpr long OFF_IN_FORWARD = 0x073f26c8;                //[Buttons]->in_forward
+constexpr long OFF_TRAVERSAL_START_TIME = 0x2af0;          //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
+constexpr long OFF_TRAVERSAL_PROGRESS = 0x2aec;            //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
+constexpr long OFF_WALL_RUN_START_TIME = 0x3584;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunStartTime
+constexpr long OFF_WALL_RUN_CLEAR_TIME = 0x3588;           //[RecvTable.DT_LocalPlayerExclusive]->m_wallRunClearTime
+constexpr long OFF_GRAPPLE_ACTIVE = 0x1e38;              //[RecvTable.DT_NPC_Titan]       Not sure
+constexpr long OFF_GRAPPLE = 0x1da8;                      //[RecvTable.DT_NPC_Titan]        Not sure
+constexpr long OFF_GRAPPLE_ATTACHED = 0x0048;              // [RecvTable.DT_GrappleData]       Not sure
+ 
+// Player
+constexpr long OFF_HEALTH = 0x0318;                           //[RecvTable.DT_Player]->m_iHealth
 constexpr long OFF_MAXHEALTH = 0x0460;                        //[RecvTable.DT_Player]->m_iMaxHealth
 constexpr long OFF_SHIELD = 0x01a0;                           //[RecvTable.DT_TitanSoul]->m_shieldHealth
 constexpr long OFF_MAXSHIELD = 0x01a4;                        //[RecvTable.DT_TitanSoul]->m_shieldHealthMax
@@ -41,13 +51,9 @@ constexpr long OFF_VIEW_ANGLES = 0x2534 - 0x14;               //[DataMap.C_Playe
 constexpr long OFF_PUNCH_ANGLES = 0x2438;                     //[DataMap.C_Player]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
 constexpr long OFF_YAW = 0x223c - 0x8;                        //m_currentFramePlayer.m_ammoPoolCount - 0x8
 constexpr long OFF_MODELNAME = 0x0030;                        //m_ModelName
-     
-    constexpr long OFF_NAMELIST = 0xC5E1150; //[Miscellaneous]->NameList o
-    constexpr long OFF_YAW = 0x221c - 0x8; //m_currentFramePlayer.m_ammoPoolCount - 0x8 o
-    constexpr long OFF_NAMEINDEX = 0x38; //nameIndex
-     
-    //weapon
-    constexpr long OFF_WEAPON_HANDLE = 0x1934;                    //[RecvTable.DT_Player]->m_latestPrimaryWeapons
+ 
+// Weapon 
+constexpr long OFF_WEAPON_HANDLE = 0x1934;                    //[RecvTable.DT_Player]->m_latestPrimaryWeapons
 constexpr long OFF_WEAPON_INDEX = 0x1778;                     //[RecvTable.DT_WeaponX]->m_weaponNameIndex
 constexpr long OFF_PROJECTILESCALE = 0x04ec + 0x19c0;         //projectile_gravity_scale + [WeaponSettingsMeta]base
 constexpr long OFF_PROJECTILESPEED = 0x04e4 + 0x19c0;         //projectile_launch_speed + [WeaponSettingsMeta]base
@@ -59,9 +65,9 @@ constexpr long OFF_SKIN = 0x0d58;                             //m_nSkin
 constexpr long OFF_TIME_BASE = 0x2088;                        //m_currentFramePlayer.timeBase
 constexpr long OFF_WEAPON_DISCARDED = 0x1599;                 //[RecvTable.DT_WeaponX]->m_discarded
 constexpr long OFF_VIEWMODELS = 0x2d18;                       //m_hViewModels
-     
-    //glow
-    constexpr long OFF_HIGHLIGHT_TYPE_SIZE = 0x34;                //                         
+ 
+//glow
+constexpr long OFF_HIGHLIGHT_TYPE_SIZE = 0x34;                //                         
 constexpr long OFF_GLOW_ENABLE = 0x26c;                       //[DT_HighlightSettings].        
 constexpr long OFF_GLOW_THROUGH_WALL = 0x26c;                 //[DT_HighlightSettings].       
 constexpr long OFF_GLOW_FIX = 0x268;                        //                                      
